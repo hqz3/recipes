73aa9fab10174f38595eabb76637a610e59edde0
hqz3 <heqzheng@gmail.com>
2022-11-07 00:42:58 -0500

    Add react-router path to Group component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import SearchBar from "./components/SearchBar";
+import Group from "./components/Group";
+import { Routes, Route } from "react-router-dom";
+import { data } from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <SearchBar />
+      <div className="container">
+        <Routes>
+          <Route path="/*" element={<Group data={data} />}></Route>
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
