bd44c7094cc25a00f61f4f321b7784210832bb89
hqz3 <heqzheng@gmail.com>
2022-12-13 00:29:27 -0500

    Create Search component to show search results

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledSearch = styled.section`
+  border: 1px solid blue;
+  width: 100%;
+  text-align: center;
+`;
+
+const Search = () => {
+  return (
+    <>
+      <StyledSearch>Search results...</StyledSearch>
+    </>
+  );
+};
+
+export default Search;
