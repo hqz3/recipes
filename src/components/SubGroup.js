1c851659449903692c2dd44dfbccc74edb97b122
hqz3 <heqzheng@gmail.com>
2022-12-12 23:49:31 -0500

    Rename Type to SubGroup for clarity. Update SubGroup routes

---
+++ src/components/SubGroup.js
@@ -0,0 +1,50 @@
+import React from "react";
+// Component
+import Recipes from "./Recipes";
+// React-Router
+import { NavLink, Route, Routes } from "react-router-dom";
+// Styled
+import styled from "styled-components";
+
+const StyledSubGroup = styled.section`
+  border: 2px solid orange;
+  background-color: var(--column-background-color);
+  height: 100%;
+  overflow-y: scroll;
+  padding: 0 40px 0 40px;
+  z-index: 2;
+`;
+
+const SubGroup = ({ subGroups }) => {
+  return (
+    <>
+      <StyledSubGroup className="custom-scrollbar expand-right">
+        {subGroups.map((subGroup) => {
+          return (
+            <NavLink key={subGroup.categoryId} to={`${subGroup.slug}`}>
+              {subGroup.name}
+            </NavLink>
+          );
+        })}
+      </StyledSubGroup>
+      <Routes>
+        {subGroups.map((subGroup) => {
+          return (
+            <Route
+              key={subGroup.categoryId}
+              path={`${subGroup.slug}/*`}
+              element={
+                <Recipes
+                  key={subGroup.categoryId}
+                  subGroupId={subGroup.categoryId}
+                />
+              }
+            />
+          );
+        })}
+      </Routes>
+    </>
+  );
+};
+
+export default SubGroup;
