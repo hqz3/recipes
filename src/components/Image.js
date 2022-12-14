5a4e3cc1e583aae8e02b68edb23c846d141729fe
hqz3 <heqzheng@gmail.com>
2022-12-14 15:49:01 -0500

    Add mobile styling

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,6 +5,10 @@ const StyledImage = styled.img`
   display: block;
   margin-bottom: 1rem;
   height: 300px;
+
+  @media screen and (max-width: 768px) {
+    height: 200px;
+  }
 `;
 
 const Image = ({ acf }) => {
